b70d727e3db08d18f194182be35433574da0cddb
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-30 07:12:53 -0500

    added userSerializer

--- product/products/serializers.py
+++ product/products/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Category, Product
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
 
 
 class CategorySerializer(serializers.ModelSerializer):
