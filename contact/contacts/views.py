3ce73118fa5a97f03977207b8259131e66eb4a7d
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-30 07:36:46 -0500

    creation of APIS for blogs

--- contact/contacts/views.py
+++ contact/contacts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+class EnviarMensajeAPIView(generics.CreateAPIView):
+    def post(self, request, *args, **kwargs):
+        # Lógica para enviar mensaje
+        # ...
+
+        return Response({"mensaje": "Mensaje enviado con éxito"})
